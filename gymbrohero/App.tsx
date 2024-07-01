9c0d5d947cbe5b3b69b8675ea27460e58273e512
ellsbee <katebroniahurley@gmail.com>
2024-07-01 16:07:14 +0100

    set up navbar structure

--- gymbrohero/App.tsx
+++ gymbrohero/App.tsx
@@ -1,11 +1,13 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Navbar from './components/Navbar'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> 
+      <Navbar />
     </View>
   );
 }
@@ -14,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
