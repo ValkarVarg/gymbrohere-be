073d9c95a5f8bc99f4eddc749ef8afa4d0da4c6a
Kathryn <kathryncreek@gmail.com>
2024-07-01 16:55:21 +0100

    remove header and fix safe area view

--- gymbrohero/App.tsx
+++ gymbrohero/App.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Navbar from './components/Navbar'; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" /> 
       <Navbar />
-    </View>
+    </SafeAreaView>
   );
 }
 
--- gymbrohero/components/Navbar.tsx
+++ gymbrohero/components/Navbar.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const Tab = createBottomTabNavigator();
+const screenOptions = { headerShown: false };
 
 export default function Navbar() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name='Home' component={HomeScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator screenOptions={screenOptions}>
+				<Tab.Screen name="Home" component={HomeScreen} />
+				<Tab.Screen name="Profile" component={ProfileScreen} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
