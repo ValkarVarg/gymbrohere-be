64416367199784f7f5a1b75a2ad9bd12a59e6088
ValkarVarg <mikewilliamstfb@gmail.com>
2024-07-02 14:38:33 +0100

    adds get request for user by id and error handling

---
+++ amplify/__tests__/endpoints.test.js
@@ -0,0 +1,47 @@
+const { seed } = require("../seed");
+const testData = require("../data/test-data");
+const db = require("../connection");
+const request = require("supertest");
+const app = require("../endpoints");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("/api/users/:userid", () => {
+  test("GET:200 sends a user object to the client", () => {
+    return request(app)
+      .get("/api/users/1")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.user).toEqual({
+          user_id: 1,
+          birthdate: "1999-03-12T00:00:00.000Z",
+          height: 180,
+          weight: 120,
+          goal: "get swole",
+          avatar_body: 2,
+          avatar_hair_shape: 1,
+          avatar_hair_colour: 1,
+          avatar_skin_colour: 1,
+          avatar_shirt_colour: 1,
+          username: "Jim",
+        });
+      });
+  });
+  test("GET:400 returns an error when a invalid userid is used", () => {
+    return request(app)
+      .get("/api/users/banana")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
+  test("GET:404 returns an error when a non-existing but valid userid is used", () => {
+    return request(app)
+      .get("/api/users/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Not Found");
+      });
+  });
+});
---
+++ amplify/models/users.model.js
@@ -0,0 +1,13 @@
+const db = require("../connection");
+
+exports.fetchUser = (id) => {
+return db.query(`SELECT users.*, usersLogin.username FROM users
+                JOIN usersLogin
+                ON users.user_id = usersLogin.user_id
+                WHERE users.user_id = $1`,[id])
+.then (({rows}) => {
+    if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
+    return rows})
+}
