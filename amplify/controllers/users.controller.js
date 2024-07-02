715076669859536de2008131fefba38a9e191ef4
ValkarVarg <mikewilliamstfb@gmail.com>
2024-07-02 12:26:14 +0100

    seeds userLogin and users test data

---
+++ amplify/controllers/users.controller.js
@@ -0,0 +1,10 @@
+const users = require("../data/test-data/users");
+// const { fetchShops } = require("../models/shops.model");
+
+// exports.getShops = (req, res, next) => {
+//     fetchShops()
+//     .then((shops) => {
+//       res.status(200).send(shops);
+//     })
+//     .catch(next);
+// };
