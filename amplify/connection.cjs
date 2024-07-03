bdbc21b4abd1bfb4c33875e82c8b9979c547c386
ValkarVarg <mikewilliamstfb@gmail.com>
2024-07-03 11:11:45 +0100

    sets up connection to prod backend

--- amplify/connection.cjs
+++ amplify/connection.cjs
@@ -1,11 +1,28 @@
 const { Pool } = require('pg');
 
-const ENV = process.env.NODE_ENV || 'development'
-const pathToEnvFile = `${__dirname}/.env.${ENV}`
-require('dotenv').config({path: pathToEnvFile})
+const ENV = process.env.NODE_ENV || 'development';
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
+require('dotenv').config({
+  path: `${__dirname}/.env.${ENV}`,
+});
 
-module.exports = new Pool();
+
+const rdsHost = process.env.RDS_HOST;
+const databaseName = process.env.RDS_DATABASE_NAME;
+const username = process.env.RDS_USERNAME;
+const password = process.env.RDS_PASSWORD;
+
+
+const config = {}
+
+if (ENV === 'production') {
+  config.host= rdsHost,
+  config.database = databaseName,
+  config.user= username,
+  config.password= password}
+
+// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+//   throw new Error('PGDATABASE or DATABASE_URL not set');
+// }
+
+module.exports = new Pool(config);
