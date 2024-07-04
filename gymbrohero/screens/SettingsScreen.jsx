73a387506bf31d8724b5b66f9b4deb5fac92bc1b
ellsbee <katebroniahurley@gmail.com>
2024-07-04 14:52:47 +0100

    creates user setup page and renders on settings page, added extra avatar image

--- gymbrohero/screens/SettingsScreen.jsx
+++ gymbrohero/screens/SettingsScreen.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { UserSetup } from "../components/UserSetup";
 
 export const SettingsScreen = () => {
   return (
     <View>
       <Text>This is the Settings Screen</Text>
+      <UserSetup />
     </View>
   );
 };
