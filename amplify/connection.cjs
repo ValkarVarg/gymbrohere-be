4c4351e8756802c8d41e46fc292baf3fd7268241
ValkarVarg <mikewilliamstfb@gmail.com>
2024-07-04 09:42:08 +0100

    setup for supabase and render

--- amplify/connection.cjs
+++ amplify/connection.cjs
@@ -6,23 +6,15 @@ require('dotenv').config({
   path: `${__dirname}/.env.${ENV}`,
 });
 
-
-const rdsHost = process.env.RDS_HOST;
-const databaseName = process.env.RDS_DATABASE_NAME;
-const username = process.env.RDS_USERNAME;
-const password = process.env.RDS_PASSWORD;
-
-
 const config = {}
 
 if (ENV === 'production') {
-  config.host= rdsHost,
-  config.database = databaseName,
-  config.user= username,
-  config.password= password}
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
-// if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-//   throw new Error('PGDATABASE or DATABASE_URL not set');
-// }
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
 
 module.exports = new Pool(config);
