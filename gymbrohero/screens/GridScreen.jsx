b5e11d5585b511a0ba965388a95ffb98bcdbfef9
ellsbee <katebroniahurley@gmail.com>
2024-07-05 17:05:16 +0100

    begins to construct grid, currently renders on storefront screen

---
+++ gymbrohero/screens/GridScreen.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native";
+const { width: screenWidth } = Dimensions.get("window");
+
+export const GridScreen = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.grid}>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+        <Text style={styles.tile}></Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: screenWidth,
+  },
+  tile: {
+    width: screenWidth / 4,
+    height: screenWidth / 4,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#C9B192",
+  },
+});
