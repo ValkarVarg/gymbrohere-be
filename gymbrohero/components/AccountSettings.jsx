82fc78545788862a680ae5df05025d1fc63c1e4f
lowndes96 <lowndes96@outlook.com>
2024-07-05 13:47:22 +0100

    settings screen buttons setup

---
+++ gymbrohero/components/AccountSettings.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import {
+  Button,
+  TextInput,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+} from "react-native";
+import { Formik } from "formik";
+
+export const AccountSettings = () => {
+    return <Text>This is the Settings Screen</Text> 
+}
--- gymbrohero/screens/SettingsScreen.jsx
+++ gymbrohero/screens/SettingsScreen.jsx
@@ -1,12 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { UserSetup } from "../components/UserSetup";
+import {AccountSettings} from '../components/AccountSettings'
+import {
+  StyleSheet,
+  Pressable
+} from 'react-native';
+
+
 
 export const SettingsScreen = () => {
+    const  [accountSettingsView, setAccountSettingsView] = useState(true)
+
+    const accountClick = () => {
+      setAccountSettingsView(true)
+    }
+    const profileClick = () => {
+      setAccountSettingsView(false)
+    }
+
   return (
-    <View>
-      <Text>This is the Settings Screen</Text>
-      <UserSetup />
+    <View style={styles.container}>
+      <View style={styles.fixToText}>
+        <Pressable onPress={accountClick}
+          style={[styles.button, accountSettingsView? styles.activeButton: styles.inactiveButton]}>
+          <Text>account settings</Text>
+        </Pressable>
+        <Pressable onPress={profileClick}
+          style={[styles.button, accountSettingsView? styles.inactiveButton: styles.activeButton]}>
+          <Text>profile settings</Text>
+        </Pressable>
+      </View>
+      <Separator />
+      {accountSettingsView? <AccountSettings/> : <UserSetup />}
     </View>
   );
 };
+
+const Separator = () => <View style={styles.separator} />;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 10
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  activeButton: {
+    backgroundColor: 'red'
+  }, 
+  inactiveButton: {
+    backgroundColor: 'blue'
+  }
+
+});
