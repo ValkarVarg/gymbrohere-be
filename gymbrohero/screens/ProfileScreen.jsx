20084a401201b6f25e251b454e718e288cf91738
ellsbee <katebroniahurley@gmail.com>
2024-07-11 09:11:24 +0100

    links profile to api and does basic styling

--- gymbrohero/screens/ProfileScreen.jsx
+++ gymbrohero/screens/ProfileScreen.jsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
+import { fetchUsers } from "../api";
 
-export const ProfileScreen = () => {
-  const [user, setUser] = useState({
-    username: "",
-    height: "",
-    weight: "",
-    age: "",
-    goal: "",
-  });
+export const ProfileScreen = ({ userId }) => {
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    setUser();
-  }, []);
+    fetchUsers(userId)
+      .then((user) => setUser(user))
+      .catch((err) => {
+        alert("you need to create an account bro!");
+        setUser(null);
+      });
+  }, [userId]);
 
   if (!user) {
     return (
@@ -24,15 +24,15 @@ export const ProfileScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.headerText}>- Check yourself out, bro! -</Text>
       <Image
-        source={require("../images/Bro.png")}
+        source={require("../images/girlchadprofile1.png")}
         style={styles.image}
         resizeMode="cover"
       />
       <Text style={styles.text}>Name: {user.username}</Text>
       <Text style={styles.text}>Height: {user.height}</Text>
       <Text style={styles.text}>Weight: {user.weight}</Text>
-      <Text style={styles.text}>Age: {user.age}</Text>
       <Text style={styles.text}>Goal: {user.goal}</Text>
     </View>
   );
@@ -52,5 +52,14 @@ const styles = StyleSheet.create({
   text: {
     marginTop: 20,
     fontSize: 18,
+    fontFamily: "pixelify-semibold",
+    color: "#69C56D",
+  },
+  headerText: {
+    marginTop: 20,
+    fontSize: 26,
+    fontFamily: "pixelify-bold",
+    color: "#69C56D",
+    paddingBottom: 15,
   },
 });
